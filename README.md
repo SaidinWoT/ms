2966298540de9560f8da2502ced3ba2c4d451f8c
SaidinWoT <SaidinWoT@gmail.com>
2013-07-08 14:36:58 -0400

    CLI config done

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Use hjkl to navigate, u/d to go up and down layers, c to check, f to flag.
 
 Todo:
 -----
-- CLI game configuration
 - Remember more things to do
--- main.c
+++ main.c
@@ -1,9 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
 #include "mine.h"
 
+Opts opts = {.pattern = 0x000000FF, .z = 1, .y = 10, .x = 10, .mines = 15};
+
+void usage() {
+    printf("Usage:\n");
+}
+
 int main(int argc, char **argv) {
-    Board *b = init();
+    Board *b;
+    char c;
+    while((c = getopt(argc, argv, "x:y:z:m:")) != -1) {
+        switch(c) {
+            case 'x':
+                sscanf(optarg, "%d", &(opts.x));
+                break;
+            case 'y':
+                sscanf(optarg, "%d", &(opts.y));
+                break;
+            case 'z':
+                sscanf(optarg, "%d", &(opts.z));
+                break;
+            case 'm':
+                sscanf(optarg, "%d", &(opts.mines));
+                break;
+            case '?':
+                usage();
+                return 1;
+        }
+    }
+    printf("%dx %dy %dz %dm\n", opts.x, opts.y, opts.z, opts.mines);
+    b = make(&opts);
+    init(b, &opts);
     play(b);
     end(b);
     return 0;
 }
-
