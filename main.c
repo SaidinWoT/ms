40a01810aa18db42aab010aafa1715401d14230e
SaidinWoT <SaidinWoT@gmail.com>
2013-07-08 16:39:59 -0400

    CLI config changes

--- main.c
+++ main.c
@@ -2,35 +2,66 @@
 #include <unistd.h>
 #include "mine.h"
 
-Opts opts = {.pattern = 0x000000FF, .z = 1, .y = 10, .x = 10, .mines = 15};
+Opts opts = {.pattern = SQUARE, .z = 1, .y = 10, .x = 10, .mines = 15};
 
 void usage() {
-    printf("Usage:\n");
+    printf("ms by Scott Frazier\n");
+    printf("\n");
+    printf("Usage: ms [options]\n");
+    printf("\n");
+    printf("-d X,Y,Z    Set dimensions with comma-delimited list\n");
+    printf("-m N        Set mine count to N\n");
+    printf("-d P        Set sweep pattern number\n");
+    printf("  1 - 2 Dimensional Square\n");
+    printf("  2 - 3 Dimensional Cube\n");
+    printf("  3 - 2 Dimensional Cross (Immediately adjacent cells only)\n");
+    printf("  4 - 3 Dimensional Cross (Immediately adjacent cells only)\n");
+    printf("  5 - 3 Dimensional Rings (Cube without corners)\n");
+    printf("\n");
+    printf("-h  Displays this information.\n");
+    printf("-v  Displays brief version information.\n");
 }
 
 int main(int argc, char **argv) {
     Board *b;
-    char c;
-    while((c = getopt(argc, argv, "x:y:z:m:")) != -1) {
+    char c, pat;
+    while((c = getopt(argc, argv, "hVd:m:p:")) != -1) {
         switch(c) {
-            case 'x':
-                sscanf(optarg, "%d", &(opts.x));
-                break;
-            case 'y':
-                sscanf(optarg, "%d", &(opts.y));
-                break;
-            case 'z':
-                sscanf(optarg, "%d", &(opts.z));
+            case '?':
+            case 'h':
+                usage();
+                return 0;
+            case 'V':
+                printf("ms by Scott Frazier\n");
+                return 0;
+            case 'd':
+                sscanf(optarg, "%d,%d,%d", &(opts.x), &(opts.y), &(opts.z));
                 break;
             case 'm':
                 sscanf(optarg, "%d", &(opts.mines));
                 break;
-            case '?':
-                usage();
-                return 1;
+            case 'p':
+                sscanf(optarg, "%d", &pat);
+                switch(pat) {
+                    case 5:
+                        opts.pattern = RINGS;
+                        break;
+                    case 4:
+                        opts.pattern = CROSS3D;
+                        break;
+                    case 3:
+                        opts.pattern = CROSS;
+                        break;
+                    case 2:
+                        opts.pattern = CUBE;
+                        break;
+                    case 1:
+                    default:
+                        opts.pattern = SQUARE;
+                        break;
+                }
         }
     }
-    printf("%dx %dy %dz %dm\n", opts.x, opts.y, opts.z, opts.mines);
     b = make(&opts);
     init(b, &opts);
     play(b);
